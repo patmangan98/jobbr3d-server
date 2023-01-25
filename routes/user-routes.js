b63663a24307222a279c6bf5d346077473281937
pat <pat.mangan@outlook.com>
2023-01-25 16:31:25 -0500

    finishing user routes

---
+++ routes/user-routes.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+const router = express.Router()
+const { createUserToken } = require('../config/auth')
+
+router.post('/sign-up', (req, res, next) => {
+    bcrypt
+        .hash(req.body.credentials.password, 10)
+        .then(hash =>  ({
+            email: req.body.credentials.email,
+            password : hash 
+        }))
+        .then(user => User.create(user))
+        .then(user => res.status(201).json({ user: user }))
+        .catch(next)
+})
+
+router.post('/sign-in', (req, res, next) => {
+    User.findOne({ email: req.body.credentials.email })
+    .then((user) => createUserToken(req, user))
+    .then((token) => res.json({ token }))
+    .catch(next)
+})
+
+module.exports = router
