ccd2114f721113feed81d8cdd9c1e28d0b7c0a30
pat <pat.mangan@outlook.com>
2023-01-25 12:10:33 -0500

    finishing config and starting server.js

---
+++ config/config.js
@@ -0,0 +1,12 @@
+const mongooseBaseName = 'jobber3d'
+
+const database = {
+    development: `mongodb://localhost/${mongooseBaseName}-development`,
+    test: `mongodb://localhost/${mongooseBaseName}-test`
+}
+
+const localDb = process.env.TESTENV ? database.test : database.development 
+
+const currentDb = process.env.DB_URI || localDb
+
+module.exports = currentDb
